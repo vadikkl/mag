b51222d4b6b2665b2b2cabf1c9fd8096835a8832
Vadik <vadikpivko@gmail.com>
2016-08-14 19:02:13 +0300

    ui added

---
+++ app/code/Mvk/InstagramWall/Ui/Component/Listing/Columns/Actions.php
@@ -0,0 +1,70 @@
+<?php
+namespace Mvk\InstagramWall\Ui\Component\Listing\Columns;
+
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Ui\Component\Listing\Columns\Column;
+use Magento\Framework\UrlInterface;
+
+/**
+ * Class Actions
+ */
+class Actions extends Column
+{
+    /**
+     * @var UrlInterface
+     */
+    protected $urlBuilder;
+
+    /**
+     * @param ContextInterface $context
+     * @param UiComponentFactory $uiComponentFactory
+     * @param UrlInterface $urlBuilder
+     * @param array $components
+     * @param array $data
+     */
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        UrlInterface $urlBuilder,
+        array $components = [],
+        array $data = []
+    ) {
+        $this->urlBuilder = $urlBuilder;
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+
+    /**
+     * Prepare Data Source
+     *
+     * @param array $dataSource
+     * @return array
+     */
+    public function prepareDataSource(array $dataSource)
+    {
+        if (isset($dataSource['data']['items'])) {
+            $storeId = $this->context->getFilterParam('store_id');
+
+            foreach ($dataSource['data']['items'] as &$item) {
+                $item[$this->getData('name')]['edit'] = [
+                    'href' => $this->urlBuilder->getUrl(
+                        'ewave_trendingnow/index/edit',
+                        ['id' => $item['entity_id'], 'store' => $storeId]
+                    ),
+                    'label' => __('Edit'),
+                    'hidden' => false,
+                ];
+                $item[$this->getData('name')]['delete'] = [
+                    'href' => $this->urlBuilder->getUrl(
+                        'ewave_trendingnow/index/delete',
+                        ['id' => $item['entity_id'], 'store' => $storeId]
+                    ),
+                    'label' => __('Delete'),
+                    'hidden' => false,
+                ];
+            }
+        }
+
+        return $dataSource;
+    }
+}
